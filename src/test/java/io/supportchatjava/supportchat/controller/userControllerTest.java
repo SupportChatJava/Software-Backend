167f7bef785cbc4560087186b4ca5445b1814577
Kayle <i493302@fhict.nl>
2023-01-07 20:02:07 +0100

    Fixed seeder emails, updated tests

--- src/test/java/io/supportchatjava/supportchat/controller/userControllerTest.java
+++ src/test/java/io/supportchatjava/supportchat/controller/userControllerTest.java
@@ -1,10 +1,15 @@
 package io.supportchatjava.supportchat.controller;
 
+import io.supportchatjava.supportchat.models.User;
+import io.supportchatjava.supportchat.repositories.userRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,17 +20,27 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(userController.class) // Loads minimal spring requirements
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(userController.class) // Loads minimal spring requirements
+@RequiredArgsConstructor
 class userControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+     private userRepository userRepository;
+
+    @BeforeEach
+    void setupController(){
+        userRepository = mock(userRepository.class);
+    }
 
     @Test
     void register() {
-        assertTrue(true);
+        User user1 = new User(1L,"Pieter@Email.com", "Pieter1");
+//        when(userRepository.save(user1)).then(returnsFirstArg());
+//        verify()
+//        assertEquals(user1.email, gottenUser.email );
     }
 
     @Test
@@ -35,10 +50,11 @@ class userControllerTest {
 
     @Test
     void users() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/user");
-        MvcResult result = mvc.perform(request).andReturn();
+        User user1 = new User(1L,"Pieter1@Email.com", "Pieter1");
+        User user2 = new User(2L,"Pieter2@Email.com", "Pieter2");
+        User user3 = new User(3L,"Pieter3@Email.com", "Pieter3");
 
-        assertEquals(1, 1);
+        assertTrue(true);
     }
 
     @Test
